7d2990268e48f02df6f7321bc77dee864762f950
William Madgin <wmadgin.cnf@gmail.com>
2024-09-25 10:43:26 -0400

    update slot

--- slotMachine/slotMachine.py
+++ slotMachine/slotMachine.py
@@ -15,22 +15,37 @@ symbol_count = {
     "🍍": 8
 }
 
+
+def check
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
-    columns = [[],[],[]]
+    columns = []
     for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
         for _ in range(rows):
-            value = symbols.choice(current_symbols)
+            value = random.choice(current_symbols)
             current_symbols.remove(value)
             column.append(value)
 
         columns.append(column)
+
+
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="\n")
 
 
 def deposit():
@@ -100,7 +115,10 @@ def main():
 
     print(f"You are betting ${bet} on {lines} line. Total bet is ${total_bet}")
 
-    print(balance, lines)
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+
+
 
 
 main()
