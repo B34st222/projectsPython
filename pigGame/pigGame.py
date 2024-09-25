3d4905416f9d3cc3d701cedd6f1b39f235834446
William Madgin <wmadgin.cnf@gmail.com>
2024-09-24 20:54:21 -0400

    pigGame done

--- pigGame/pigGame.py
+++ pigGame/pigGame.py
@@ -1,5 +1,6 @@
 import random
 
+
 def roll():
     min_value = 1
     max_value = 6
@@ -7,9 +8,47 @@ def roll():
 
     return roll
 
+
 while True:
     players = input("number of players: (2-4) ")
     if players.isdigit():
         players = int(players)
         if 2 <= players <= 4:
             break
+        else:
+            print("between 2 and 4")
+    else:
+        print("invalid")
+
+max_score = 50
+player_score = [0 for _ in range(players)]
+
+while max(player_score) < max_score:
+
+    for player_idx in range(players):
+        print("player ", player_idx + 1, "turn started")
+        print("Your total score is ", player_score[player_idx], "\n")
+        current_score = 0
+
+        while True:
+
+            should_roll = input("would you like to roll(y)")
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("you rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("you rolled a ", value)
+
+            print("current score: ", current_score)
+
+        player_score[player_idx] += current_score
+        print("your total score is: ", player_score[player_idx])
+
+max_score = max(player_score)
+winning_idx = player_score.index(max_score)
+print("Player number", winning_idx + 1, "is the winner")
