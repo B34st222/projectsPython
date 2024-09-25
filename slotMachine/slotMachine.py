553a56c37f3ae33fbfcc9c0704e17edf2e1a80ef
William Madgin <wmadgin.cnf@gmail.com>
2024-09-24 22:04:57 -0400

    slotMachine continue

---
+++ slotMachine/slotMachine.py
@@ -0,0 +1,106 @@
+import random
+import emoji
+
+MAX_LINE = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "7️⃣": 2,
+    "🍒": 4,
+    "🍉": 6,
+    "🍍": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [[],[],[]]
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = symbols.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+
+def deposit():
+    while True:
+        amount = input("What would you like to deposit ? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Enter an amount greater than 0")
+        else:
+            print("Enter valid number")
+
+    return amount
+
+
+def get_number_of_line():
+    while True:
+        lines = input("Enter the number of lines you want to bet on (1-" + str(MAX_LINE) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 0 < lines <= MAX_LINE:
+                break
+            else:
+                print("Enter a number of line between 1 and " + str(MAX_LINE))
+        else:
+            print("Enter valid number")
+
+    return lines
+
+
+def get_bet():
+    while True:
+        bet = input("How much do you want to bet? $")
+        if bet.isdigit():
+            bet = int(bet)
+            if bet > MAX_BET:
+                bet = MAX_BET
+                print("Your bet is $" + str(bet))
+                break
+            elif bet < MIN_BET:
+                bet = MIN_BET
+                print("Your bet is $" + str(bet))
+                break
+            else:
+                print("Your bet is $" + str(bet))
+                break
+
+        else:
+            print("Enter valid number")
+
+    return bet
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_line()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print("You do not have enough")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} line. Total bet is ${total_bet}")
+
+    print(balance, lines)
+
+
+main()
